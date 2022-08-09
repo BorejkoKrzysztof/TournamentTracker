47182cefb3d8afb53bcacddb7ad5e933f2a7fdef
KrzysztofBorejko <krzysiek.13.krzysiek@gmail.com>
2022-08-09 17:27:10 +0200

    Work 09.08.22

--- TrackerLibrary/DataAccess/SqlConnector.cs
+++ TrackerLibrary/DataAccess/SqlConnector.cs
@@ -1,9 +1,19 @@
-﻿using System;
+﻿using Dapper;
+using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using TrackerLibrary.Models;
+
+	//@PlaceNumber int,
+	//@PlaceName nvarchar(50),
+	//@PrizeAmount money,
+ //   @PrizePercentage float,
+ //   @Id int = 0 output
+
 
 namespace TrackerLibrary.DataAccess
 {
@@ -17,7 +27,21 @@ namespace TrackerLibrary.DataAccess
         /// <exception cref="NotImplementedException"></exception>
         public PrizeModel CreatePrize(PrizeModel model)
         {
-            throw new NotImplementedException();
+            using (IDbConnection connection = new SqlConnection(GlobalConfig.CnnString("Tournaments")))
+            {
+                var p = new DynamicParameters();
+                p.Add("@PlaceNumber", model.PlaceNumber);
+                p.Add("@PlaceName", model.PlaceName);
+                p.Add("@PrizeAmount", model.PrizeAmount);
+                p.Add("@PrizePercentage", model.PrizePercentage);
+                p.Add("@Id", 0, dbType: DbType.Int32, direction: ParameterDirection.Output);
+
+                connection.Execute("dbo.spPrizes_Insert", p, commandType: CommandType.StoredProcedure);
+
+                model.Id = p.Get<int>("@Id");
+
+                return model;
+            }
         }
     }
 }
